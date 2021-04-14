672a9f98c230e6b7e3b5a9e0726221f739224ac2
maheshpawar888 <maheshpawar83083.com>
2021-04-14 18:43:58 +0530

    finalAuctionSolved

--- src/components/FinalAuction.js
+++ src/components/FinalAuction.js
@@ -8,13 +8,23 @@ const FinalAuction = () =>{
     const id = useParams()
 
         const [data, setData] = useState({
-          items: []
+          items: [],
+          itemName:'AAA',
+          itemImg:'',
+          reservePrice:2000
       })
+      const { itemName,itemImg,reservePrice } = data;
+    const onChange = e => setData({...data,[e.target.name]:e.target.value}) 
 
       const getItem = () => {
           axios.get("http://localhost:5000/getItems/"+id.id).then((res) => {
               setData({items: res.data})
-              console.log(res.data)
+              setData({
+                itemName:res.data[0].itemName,
+                itemImg:res.data[0].itemImg,
+                reservePrice:res.data[0].reservePrice
+              })
+              console.log(res.data[0].itemName)
           })
       }
 
@@ -28,22 +38,26 @@ const FinalAuction = () =>{
     return (
         <div>
           <CustomerDash/> 
-            {/* <h1>Show Auction</h1> */}
 
             <div className="row">
             <div className="col-6">
-                <img id="image" src="image.png"/>
+                <img id="image" src={itemImg}/>
                 <p id="image-id">Item ID - 1</p>
             </div>
             <div className="col-6">
                 <table id="details">
                     <tr>
                         <td><h5><b>Item Name :</b></h5></td>
-                        <td><h6>{data.items[0].itemName}</h6></td>
+                        <td><h6 name="itemName" value={itemName} 
+                        onChange={e=>onChange(e)}
+                        >{itemName}</h6></td>
                     </tr>
                     <tr>
                         <td><h5><b>Reserved Price :</b></h5></td>
-                        <td><h6>20000</h6></td>
+                        <td><h6
+                        name="reservePrice" value={reservePrice} 
+                        onChange={e=>onChange(e)}
+                        >{reservePrice}</h6></td>
                     </tr>
                     <tr>
                         <td><h5><b>Your cost :</b></h5></td>
